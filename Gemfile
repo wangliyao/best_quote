975b4cc5c42a47783347165ba75b85ceeed2026b
wangliyao <755210570@qq.com>
2018-08-07 23:07:40 +0800

    rulers sqlite model

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://gems.ruby-china.org'
+
+
+gem 'rulers', path: '../rulers'
+
+group :development, :test do
+  gem 'rerun', '~> 0.8.0'
+end
---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,36 @@
+class QuotesController < Rulers::Controller
+
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
+  def a_quote
+    render :a_quote, :noun => :winking
+  end
+
+  def quote_1
+    quote_1 = Rulers::Model::FileModel.find(1)
+    render :quote, :obj => quote_1
+  end
+
+  def show
+    quote = FileModel.find(params["id"])
+    ua = request.user_agent
+    render_response :quote, :obj => quote, :ua => ua
+  end
+
+  def new_quote
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "A picture is worth a thousand pixels",
+      "attribution" => "Me"
+    }
+    m = FileModel.create attrs
+    render :quote, :obj => m
+  end
+
+  def exception
+    raise "It's a bad one"
+  end
+end
