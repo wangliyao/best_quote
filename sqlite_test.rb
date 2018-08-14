f16a3f441201097ee071a3a846ac6579b4dfee4a
wangliyao <755210570@qq.com>
2018-08-14 22:11:15 +0800

    sqlite_model

--- sqlite_test.rb
+++ sqlite_test.rb
@@ -8,3 +8,15 @@ STDERR.puts MyTable.schema.inspect
 mt = MyTable.create "title" => "It happened", "posted" => 1, "body" => "It did!"
 mt = MyTable.create "title" => "I saw it!"
 puts "Count: #{MyTable.count}"
+
+top_id = mt["id"].to_i
+(1..top_id).each do |id|
+  mt_id = MyTable.find(id)
+  puts "Found title #{mt_id["title"]}."
+end
+
+#Create now
+mt = MyTable.create "title" => "I saw it again!"
+mt["title"] = "I really did!"
+mt2 = MyTable.find mt["id"]
+puts "Title: #{mt2["title"]}"
